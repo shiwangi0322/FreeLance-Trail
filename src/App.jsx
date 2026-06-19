e1333e9611bbf604cc2c220ce4c1c37ad4369423
shiwangi0322 <shiwangipandey0003@gmail.com>
2026-06-20 01:47:48 +0530

    Setup new frontend structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import AuthContainer from "./components/AuthContainer";
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-gray-300 to-blue-200">
       <AuthContainer />
     </div>
   );
